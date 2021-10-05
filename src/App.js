362e1353023eafb267e2d322c0941f5479590938
farukwebcode21 <farukahmadmk@yahoo.com>
2021-10-05 23:49:43 +0600

    react start

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
 
+
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Person name="Faruk"/>
+    <Person/>
+    <Person/>
+    <Person/>
     </div>
   );
+}
+
+function Person(props) {
+  console.log(props);
+  return (
+    <div className="person">
+    <h1>Shakib al Hasan</h1>
+    <h4>Professor:Crickter</h4>
+  </div>
+  )
+  
 }
 
 export default App;
